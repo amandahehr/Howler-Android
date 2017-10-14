7f142549325a7d117a8c5cbdb63b31ca5dbf4504
Amanda Hehr <amanhehr@gmail.com>
2017-10-14 14:13:09 -0700

    Timer functionality complete

--- app/src/main/java/com/example/amanda/howler/MainActivity.java
+++ app/src/main/java/com/example/amanda/howler/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.amanda.howler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Spinner spinner1, spinner2;
+    private Button startTrackingButton;
+    private EditText startLocation;
+    private EditText endLocation;
+    private EditText timerHoursEditText;
+    private EditText timerMinutesEditText;
+    private int timerStart;
+    private int timerHours;
+    private int timerMinutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Widget References
+        startTrackingButton = (Button) findViewById(R.id.startTrackingButton);
+        startLocation = (EditText) findViewById(R.id.startLocationEditText);
+        endLocation = (EditText) findViewById(R.id.endLocationEditText);
+        timerHoursEditText = (EditText) findViewById(R.id.hourEditText);
+        timerMinutesEditText = (EditText) findViewById(R.id.minuteEditText);
+
+        //Listeners
+        startTrackingButton.setOnClickListener(this);
+
+        //Variables
+        timerHours = 0;
+        timerMinutes = 0;
+
+        //Functions
+        addSpinnerItems();
+    }
+
+    //Adds contacts to dropdown
+    public void addSpinnerItems() {
+        //Get spinners
+        spinner1 = (Spinner) findViewById(R.id.contactListSpinner1);
+        spinner2 = (Spinner) findViewById(R.id.contactListSpinner2);
+
+        //Create contact list
+        List<String> contactList1 = new ArrayList<String>();
+        contactList1.add("-- Select Primary Contact --");
+        contactList1.add("Mom");
+        contactList1.add("Dad");
+        contactList1.add("Sarah");
+        contactList1.add("Joe");
+
+        //Create array adapters
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, contactList1);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Set spinner items
+        spinner1.setAdapter(dataAdapter);
+    }
+
+    //OnClick Functions
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.startTrackingButton:
+                //Compute timer time
+                timerHours = Integer.parseInt(timerHoursEditText.getText().toString());
+                timerMinutes = Integer.parseInt(timerMinutesEditText.getText().toString());
+                timerStart = timerHours*3600000 + timerMinutes*60000;
+                //Send timer time to next activity
+                Intent i = new Intent(getApplicationContext(), TrackingTimer.class);
+                i.putExtra("TIMER_VAL", timerStart);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/amanda/howler/TrackingTimer.java
@@ -0,0 +1,109 @@
+package com.example.amanda.howler;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TrackingTimer extends AppCompatActivity implements View.OnClickListener {
+    public int counter;
+    public TextView countdownVal;
+    public TextView timerHours;
+    public TextView timerMins;
+    public TextView timerSecs;
+    public int timerStart;
+    public int hours;
+    public int minutes;
+    public int seconds;
+    public Button stopTrackingButton;
+    public Button addTimeButton;
+    public CountDownTimer theTimer;
+    public double h;
+    public double m;
+    public double s;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking_timer);
+
+        //Get widget references
+        countdownVal = (TextView) findViewById(R.id.countdownTextView);
+        timerHours = (TextView) findViewById(R.id.hourTextView);
+        timerMins = (TextView) findViewById(R.id.minTextView);
+        timerSecs = (TextView) findViewById(R.id.secTextView);
+        stopTrackingButton = (Button) findViewById(R.id.stopTrackingButton);
+        addTimeButton = (Button) findViewById(R.id.addTimeButton);
+
+        //Listeners
+        stopTrackingButton.setOnClickListener(this);
+        addTimeButton.setOnClickListener(this);
+
+        //Get passed timer value
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            timerStart = extras.getInt("TIMER_VAL");
+        }
+
+        //Set text/variables
+        counter = timerStart;
+        countdownVal.setText(Integer.toString(timerStart));
+
+        //start timer
+        theTimer = new trackingTimer(timerStart, 1000).start();
+
+    }
+
+    //OnClick functions
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addTimeButton:
+                addTime();
+                break;
+            case R.id.stopTrackingButton:
+                stopTracking();
+                break;
+        }
+    }
+
+    public class trackingTimer extends CountDownTimer{
+        public trackingTimer(long milisInFuture, long countDownInterval){
+            super(milisInFuture,countDownInterval);
+            counter = (int) milisInFuture;
+        }
+        @Override
+        public void onTick(long millisUntilFinished){
+            //set hours text
+            h = counter*2.778e-7;
+            hours = (int) h;
+            timerHours.setText(String.valueOf(hours));
+            //set minutes text
+            m = counter*1.667e-5 - hours*60;
+            minutes = (int) m;
+            timerMins.setText(String.valueOf(minutes));
+            //set seconds text
+            s = counter*0.001 - minutes*60 - hours*3600;
+            seconds = (int) s;
+            timerSecs.setText(String.valueOf(seconds));
+
+            //count down
+            counter-=1000;
+        }
+        @Override
+        public void onFinish(){
+            countdownVal.setText("FINISH!!");
+        }
+    }
+
+    public void addTime(){
+        theTimer.cancel();
+        theTimer = new trackingTimer(counter + 5*60000, 1000).start();
+    }
+
+    public void stopTracking(){
+        theTimer.cancel();
+    }
+}
